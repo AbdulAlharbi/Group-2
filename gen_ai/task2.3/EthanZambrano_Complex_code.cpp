ee6454930e9b82b7cde9ac3fae14c26314c8de9d
Ethan Zambrano <161177620+EthanZ23@users.noreply.github.com>
2025-03-01 02:12:19 -0800

    Task 2.3 complex code ex

--- gen_ai/task2.3/EthanZambrano_Complex_code.cpp
+++ gen_ai/task2.3/EthanZambrano_Complex_code.cpp
@@ -22,3 +22,39 @@ int manhattanDistance(const State& state, const int goal[][SIZE]) {
     }
     return distance;
  }
+// ------------------------------------------------------------------------
+// THIS IS JUST AN EXAMPLE FROM A LARGER CODE OF MINE
+// CHATGPT heuristic function / approacj is the manhattanDistance / algo
+// #include <cmath>
+// #include <unordered_map>
+
+const int SIZE = 3;
+
+struct Position {
+    int row, col;
+};
+
+int manhattanDistance(const State& state, const int goal[][SIZE]) {
+    int distance = 0;
+    std::unordered_map<int, Position> goalPositions;
+
+    // Pre-compute goal positions
+    for (int i = 0; i < SIZE; ++i) {
+        for (int j = 0; j < SIZE; ++j) {
+            goalPositions[goal[i][j]] = {i, j};
+        }
+    }
+
+    // Calculate Manhattan distance
+    for (int i = 0; i < SIZE; ++i) {
+        for (int j = 0; j < SIZE; ++j) {
+            int value = state.puzzle[i][j];
+            if (value != 0) {
+                Position goalPos = goalPositions[value];
+                distance += abs(i - goalPos.row) + abs(j - goalPos.col);
+            }
+        }
+    }
+
+    return distance;
+}
